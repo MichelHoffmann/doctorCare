f5c4b896646a612cab92e266dbca3e5da9efe8bf
Michel Hoffmann <69813855+MichelHoffmann@users.noreply.github.com>
2022-06-08 16:44:51 -0500

    z

--- NLW-return/main.js
+++ NLW-return/main.js
@@ -1,4 +1,6 @@
 let nav = document.getElementById('navigation')
+let menu = document.getElementsByClassName('open-menu')
+let corpo = document.getElementsByTagName('body')
 
 function onScroll() {
   if (scrollY > 0) {
@@ -6,4 +8,12 @@ function onScroll() {
   } else {
     nav.classList.remove('scroll')
   }
+}
+
+function openMenu() {
+  corpo.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  corpo.classList.remove('menu-expanded')
 }
