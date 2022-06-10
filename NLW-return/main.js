511122127c5bbb8b83cee514138378c02184e4a5
Michel Hoffmann <69813855+MichelHoffmann@users.noreply.github.com>
2022-06-10 16:25:15 -0500

    Finish day 3 of NLW

--- NLW-return/main.js
+++ NLW-return/main.js
@@ -17,3 +17,17 @@ function openMenu() {
 function closeMenu() {
   corpo.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: '700',
+}).reveal(`#home,
+#home img, 
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#aboyt .content,`)
