0714eb86504bf897b6451367e6c1d87ecab4d5d1
Michel Hoffmann <69813855+MichelHoffmann@users.noreply.github.com>
2022-06-11 17:24:43 -0500

    Quase finalizando a aula 4

--- NLW-return/main.js
+++ NLW-return/main.js
@@ -1,12 +1,23 @@
 let nav = document.getElementById('navigation')
 let menu = document.getElementsByClassName('open-menu')
 let corpo = document.getElementById('body')
+let scrollitem = document.getElementById('BackToTop')
 
 function onScroll() {
-  if (scrollY > 0) {
-    nav.classList.add('scroll')
-  } else {
-    nav.classList.remove('scroll')
+  function navScroll() {
+    if (scrollY > 0) {
+      nav.classList.add('scroll')
+    } else {
+      nav.classList.remove('scroll')
+    }
+  }
+
+  function BackToTop() {
+    if (scrollY > 550) {
+      scrollitem.classList.add('BackToTopOn')
+    } else {
+      scrollitem.classList.remove('BackToTopOn')
+    }
   }
 }
 
@@ -24,7 +35,7 @@ ScrollReveal({
   duration: '700',
 }).reveal(`#home,
 #home img, 
-#home .stats,
+#home .stat,
 #services,
 #services header,
 #services .card,
